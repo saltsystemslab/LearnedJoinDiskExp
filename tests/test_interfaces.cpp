d85b4683a4b40e766d85fb7d5d719924a252ea18
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-05-18 13:55:45 -0400

    Moving int iterator to header

--- tests/test_interfaces.cpp
+++ tests/test_interfaces.cpp
@@ -17,63 +17,6 @@ public:
   }
 };
 
-class IntArrayIterator : public Iterator<int> {
-public:
-  IntArrayIterator(int *a, int n) {
-    this->a = a;
-    this->cur = 0;
-    this->n = n;
-  }
-  ~IntArrayIterator() { delete a; }
-  bool valid() const override { return cur < n; }
-  void next() override {
-    assert(valid());
-    cur++;
-  }
-  int peek(uint64_t pos) const override { return a[pos]; }
-  void seek(int item) override {
-    for (int i = 0; i < n; i++) {
-      if (a[i] > item) {
-        cur = i;
-        return;
-      }
-    }
-    cur = n;
-  }
-  void seekToFirst() override { cur = 0; }
-  int key() const override { return a[cur]; }
-  uint64_t current_pos() const override { return cur; }
-  double guessPosition(int target_key) {
-    for (int i = 0; i < n; i++) {
-      if (a[i] > target_key) {
-        return cur;
-      }
-    }
-    return cur;
-  }
-
-private:
-  int *a;
-  int cur;
-  int n;
-};
-
-class IntArrayIteratorBuilder : public IteratorBuilder<int> {
-public:
-  IntArrayIteratorBuilder(int n) {
-    this->a = new int[n];
-    this->cur = 0;
-    this->n = n;
-  }
-  void add(int t) override { a[cur++] = t; }
-  IntArrayIterator *finish() override { return new IntArrayIterator(a, n); }
-
-private:
-  int *a;
-  int n;
-  int cur;
-};
-
 int main() {
   IntArrayIteratorBuilder *builder1 = new IntArrayIteratorBuilder(10);
   for (int i = 0; i < 10; i++) {
--- tests/test_learned_merger.cpp
+++ tests/test_learned_merger.cpp
@@ -17,59 +17,6 @@ public:
   }
 };
 
-class ArrayIntIterator : public Iterator<int> {
-public:
-  ArrayIntIterator(int *a, int n) {
-    this->a = a;
-    this->cur = 0;
-    this->n = n;
-  }
-  ~ArrayIntIterator() { delete a; }
-  bool valid() const override { return cur < n; }
-  void next() override { cur++; }
-  int peek(uint64_t pos) const override { return a[pos]; }
-  void seek(int item) override {
-    for (int i = 0; i < n; i++) {
-      if (a[i] > item) {
-        cur = i;
-        return;
-      }
-    }
-    cur = n;
-  }
-  void seekToFirst() override { cur = 0; }
-  int key() const override { return a[cur]; }
-  uint64_t current_pos() const override { return cur; }
-  double guessPosition(int target_key) {
-    for (int i = 0; i < n - 1; i++) {
-      if (a[i] > target_key) {
-        return cur;
-      }
-    }
-    return cur;
-  }
-
-private:
-  int *a;
-  int cur;
-  int n;
-};
-
-class IntArrayIteratorBuilder : public IteratorBuilder<int> {
-public:
-  IntArrayIteratorBuilder(int n) {
-    this->a = new int[n];
-    this->cur = 0;
-    this->n = n;
-  }
-  void add(int t) override { a[cur++] = t; }
-  ArrayIntIterator *finish() override { return new ArrayIntIterator(a, n); }
-
-private:
-  int *a;
-  int n;
-  int cur;
-};
 
 int main() {
   IntArrayIteratorBuilder *builder1 = new IntArrayIteratorBuilder(10);
