f0f72ce3f785f6b55fe1bfe7239cb4f99093ba9e
Susmitha <susmitharaj30@gmail.com>
2023-05-24 17:59:02 -0400

    tested ycsb keys

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -24,13 +24,16 @@ static bool FLAGS_disk_backed = false;
 static bool FLAGS_print_result = false;
 static const char *FLAGS_num_keys = "10,10";
 static const char *FLAGS_DB_dir = "./DB";
+static const char *FLAGS_file_names = "";
 
-vector<uint64_t> generate_keys(uint64_t num_keys, uint64_t universe) {
-  std::random_device rd;   // a seed source for the random number engine
-  std::mt19937 gen(rd());  // mersenne_twister_engine seeded with rd()
+vector<uint64_t> generate_keys(uint64_t num_keys, uint64_t universe)
+{
+  std::random_device rd;  // a seed source for the random number engine
+  std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
   std::uniform_int_distribution<uint64_t> distrib(1, universe);
   vector<uint64_t> keys;
-  for (int i = 0; i < num_keys; i++) {
+  for (int i = 0; i < num_keys; i++)
+  {
     uint64_t key = distrib(gen);
     keys.push_back(key);
   }
@@ -38,93 +41,156 @@ vector<uint64_t> generate_keys(uint64_t num_keys, uint64_t universe) {
   return keys;
 }
 
-string to_fixed_size_key(uint64_t key_value, int key_size) {
+string to_fixed_size_key(uint64_t key_value, int key_size)
+{
   string key = to_string(key_value);
   string result = string(key_size - key.length(), '0') + key;
   return std::move(result);
 }
 
-bool is_flag(const char *arg, const char *flag) {
+bool is_flag(const char *arg, const char *flag)
+{
   return strncmp(arg, flag, strlen(flag)) == 0;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   char db_dir[100];
-  for (int i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++)
+  {
     double m;
     long long n;
     char junk;
-    if (sscanf(argv[i], "--num_of_lists=%lld%c", &n, &junk) == 1) {
+    if (sscanf(argv[i], "--num_of_lists=%lld%c", &n, &junk) == 1)
+    {
       FLAGS_num_of_lists = n;
-    } else if (is_flag(argv[i], "--num_keys=")) {
+    }
+    else if (is_flag(argv[i], "--num_keys="))
+    {
       FLAGS_num_keys = argv[i] + strlen("--num_keys=");
-    } else if (sscanf(argv[i], "--key_size_bytes=%lld%c", &n, &junk) == 1) {
+    }
+    else if (sscanf(argv[i], "--key_size_bytes=%lld%c", &n, &junk) == 1)
+    {
       FLAGS_key_size_bytes = n;
-    } else if (sscanf(argv[i], "--use_disk=%lld%c", &n, &junk) == 1) {
+    }
+    else if (sscanf(argv[i], "--use_disk=%lld%c", &n, &junk) == 1)
+    {
       FLAGS_disk_backed = n;
-    } else if (sscanf(argv[i], "--print_result=%lld%c", &n, &junk) == 1) {
+    }
+    else if (sscanf(argv[i], "--print_result=%lld%c", &n, &junk) == 1)
+    {
       FLAGS_print_result = n;
-    } else {
+    }
+    else if (is_flag(argv[i], "--file_names="))
+    {
+      FLAGS_file_names = argv[i] + strlen("--file_names=");
+    }
+    else
+    {
       printf("WARNING: unrecognized flag %s\n", argv[i]);
     }
   }
 
-  if (FLAGS_disk_backed) {
+  if (FLAGS_disk_backed)
+  {
     system("mkdir -p DB");
   }
 
   vector<int> num_keys;
-  stringstream ss(FLAGS_num_keys);
+  vector<std::string> filenames;
 
-  while (ss.good()) {
-    string substr;
-    getline(ss, substr, ',');
-    num_keys.push_back(stoi(substr));
+  if (FLAGS_file_names != "")
+  {
+    stringstream s(FLAGS_file_names);
+    while (s.good())
+    {
+      string substr;
+      getline(s, substr, ',');
+      filenames.push_back(substr);
+      fstream new_file;
+      new_file.open(substr, ios::in);
+      if (new_file.is_open())
+      {
+        string s;
+        int number_of_lines = 0;
+        while (getline(new_file, s))
+        {
+          number_of_lines++;
+        }
+        num_keys.push_back(number_of_lines);
+      }
+    }
   }
-  int num_of_lists = num_keys.size();
+  else
+  {
+    stringstream ss(FLAGS_num_keys);
+
+    while (ss.good())
+    {
+      string substr;
+      getline(ss, substr, ',');
+      num_keys.push_back(stoi(substr));
+    }
+  }
+  int num_of_lists = FLAGS_num_of_lists;
   Iterator<Slice> **iterators = new Iterator<Slice> *[num_of_lists];
   int total_num_of_keys = 0;
-  std::vector<std::string> files{"src/117+118.txt", "src/105.txt"};
-  for (int i = 0; i < num_of_lists; i++) {
+  // std::vector<std::string> files{"src/117+118.txt", "src/105.txt"};
+  for (int i = 0; i < num_of_lists; i++)
+  {
     IteratorBuilder<Slice> *builder;
-    if (FLAGS_disk_backed) {
+    if (FLAGS_disk_backed)
+    {
       std::string fileName = "./DB/" + std::to_string(i) + ".txt";
       std::cout << fileName << std::endl;
       builder = new FixedSizeSliceFileIteratorBuilder(
           fileName.c_str(), BUFFER_SIZE, FLAGS_key_size_bytes, i);
-    } else {
+    }
+    else
+    {
       builder = new SliceArrayBuilder(num_keys[i], FLAGS_key_size_bytes, i);
     }
-    fstream new_file;
-    new_file.open(files[i], ios::in); 
-    std::cout<<"before file open"<<std::endl;
-    if (new_file.is_open()) { 
-      std::cout<<"after file open"<<std::endl;
+    if (FLAGS_file_names != "")
+    {
+      fstream new_file;
+      new_file.open(filenames[i], ios::in);
+      if (new_file.is_open())
+      {
         string s;
-        // Read data from the file object and put it into a string.
-        while (getline(new_file, s)) { 
-            int strpos = s.find(" ");
-            string key = s.substr(0, strpos);
-            key.erase(remove(key.begin(), key.end(), '\'' ), key.end());
-            std::cout<<key<<std::endl;
-            builder->add(Slice(key.c_str(), FLAGS_key_size_bytes));
+        int number_of_lines = 0;
+        while (getline(new_file, s))
+        {
+          number_of_lines++;
+          int strpos = s.find(" ");
+          string key = s.substr(0, strpos);
+          key.erase(remove(key.begin(), key.end(), '\''), key.end());
+          // std::cout<<"\""<<key<<"\""<<" "<<key.size()<<std::endl;
+          builder->add(Slice(key.c_str(), FLAGS_key_size_bytes));
         }
+      }
     }
-    //auto keys = generate_keys(num_keys[i], FLAGS_universe_size);
-    // for (int j = 0; j < num_keys[i]; j++) {
-    //   std::string key = to_fixed_size_key(keys[j], FLAGS_key_size_bytes);
-    //   builder->add(Slice(key.c_str(), FLAGS_key_size_bytes));
-    // }
+    else
+    {
+      auto keys = generate_keys(num_keys[i], FLAGS_universe_size);
+      for (int j = 0; j < num_keys[i]; j++)
+      {
+        std::string key = to_fixed_size_key(keys[j], FLAGS_key_size_bytes);
+        builder->add(Slice(key.c_str(), FLAGS_key_size_bytes));
+      }
+    }
     iterators[i] = builder->finish();
     total_num_of_keys += num_keys[i];
   }
 
   Comparator<Slice> *c = new SliceComparator();
   IteratorBuilder<Slice> *resultBuilder;
-  if (FLAGS_disk_backed) {
+  if (FLAGS_disk_backed)
+  {
     resultBuilder = new FixedSizeSliceFileIteratorBuilder(
         "./DB/result.txt", BUFFER_SIZE, FLAGS_key_size_bytes, 0);
-  } else {
+  }
+  else
+  {
     resultBuilder =
         new SliceArrayBuilder(total_num_of_keys, FLAGS_key_size_bytes, 0);
   }
@@ -145,10 +211,13 @@ int main(int argc, char **argv) {
                       merge_end - merge_start)
                       .count();
 
-  if (FLAGS_print_result) {
-    while (result->valid()) {
+  if (FLAGS_print_result)
+  {
+    while (result->valid())
+    {
       Slice k = result->key();
-      for (int i = 0; i < FLAGS_key_size_bytes; i++) {
+      for (int i = 0; i < FLAGS_key_size_bytes; i++)
+      {
         printf("%c", k.data_[i]);
       }
       printf("\n");
