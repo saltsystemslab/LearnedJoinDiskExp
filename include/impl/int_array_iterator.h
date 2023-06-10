025caa94a979594deee98a7ca13d2282fdbdee64
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-06-09 20:57:20 -0400

    Adding an int iterator

---
+++ include/impl/int_array_iterator.h
@@ -0,0 +1,107 @@
+#ifndef INT_ARRAY_H
+#define INT_ARRAY_H
+
+#include "iterator.h"
+
+template <class T>
+class IntArrayIterator : public Iterator<T> {
+ public:
+  IntArrayIterator(T *a, uint64_t num_keys, std::string id)
+      : id_(id), num_keys_(num_keys), cur_(0), a_(a) {}
+  ~IntArrayIterator() { delete a_; }
+  bool valid() const override { return cur_ < num_keys_; };
+  void next() override { cur_++; };
+  T peek(uint64_t pos) const override { return a_[pos]; };
+  void seekToFirst() override { cur_ = 0; };
+  T key() override { return a_[cur_]; };
+  uint64_t current_pos() const override { return cur_; };
+  std::string id() override { return id_; }
+  uint64_t num_keys() const override { return num_keys_; }
+
+ private:
+  T *a_;
+  uint64_t num_keys_;
+  uint64_t cur_;
+  std::string id_;
+};
+
+template <class T>
+class BulkReadIntArrayIterator : public BulkReadIterator<T> {
+ public:
+  BulkReadIntArrayIterator(T *a, uint64_t num_keys, std::string id)
+      : id_(id), num_keys_(num_keys), cur_(0), a_(a) {}
+  virtual bool valid() const override { return cur_ < num_keys_; }
+  virtual uint64_t current_pos() const override { return cur_; }
+  virtual uint64_t num_keys() const override { return num_keys_; }
+  virtual T key() override { return a_[cur_]; }
+  virtual uint64_t bulkReadAndForward(uint64_t num_keys, char **data,
+                                      uint64_t *len) {
+    *data = (char *)(a_ + cur_);
+    *len = num_keys_ * sizeof(T);
+    cur_ += num_keys_;
+    return num_keys_;
+  };
+  virtual std::string id() { return id_; }
+
+ private:
+  T *a_;
+  uint64_t num_keys_;
+  uint64_t cur_;
+  std::string id_;
+};
+
+template <class T>
+class IntArrayBuilder : public IteratorBuilder<T> {
+ public:
+  IntArrayBuilder(uint64_t num_keys, std::string id)
+      : a_(new T[num_keys]),
+        cur_(0),
+        num_keys_(num_keys),
+        id_(id),
+        finished_(false){};
+  void add(const T &t) override { a_[cur_++] = t; }
+  Iterator<T> *build() override {
+    return new IntArrayIterator<T>(a_, num_keys_, id_);
+  };
+  BulkReadIterator<T> *buildBulkIterator() override {
+    return new BulkReadIntArrayIterator<T>(a_, num_keys_, id_);
+  };
+
+ private:
+  T *a_;
+  uint64_t num_keys_;
+  uint64_t cur_;
+  std::string id_;
+  bool finished_;
+};
+
+template <class T>
+class BulkAddIntArrayIteratorBuilder : public BulkAddIteratorBuilder<T> {
+ public:
+  BulkAddIntArrayIteratorBuilder(uint64_t num_keys_, std::string id)
+      : a_(new T[num_keys_]), id_(id) {}
+  void bulkAdd(BulkReadIterator<T> *iter, uint64_t keys_to_add) override {
+    T *buffer;
+    uint64_t len;
+    while (keys_to_add > 0) {
+      uint64_t keys_added =
+          iter->bulkReadAndForward(keys_to_add, &buffer, &len);
+      keys_to_add -= keys_added;
+      for (uint64_t i = 0; i < keys_added; i++) {
+        a_[cur_++] = buffer[i];
+      }
+    }
+  }
+  Iterator<T> *finish() override {
+    return new IntArrayBuilder<T>(a_, num_keys_, id_);
+  }
+
+ private:
+  T *a_;
+  uint64_t num_keys_;
+  uint64_t cur_;
+  std::string id_;
+  bool finished_;
+};
+
+#endif
---
+++ include/impl/int_comparator.h
@@ -0,0 +1,21 @@
+#ifndef INT_COMPARATOR_H
+#define INT_COMPARATOR_H
+
+#include "comparator.h"
+#include "config.h"
+#include "slice.h"
+#include <cstring>
+
+template <class T>
+class IntComparator : public Comparator<T> {
+public:
+  int compare(const T &a, const T &b) override {
+    if (a == b)
+      return 0;
+    if (a < b)
+      return -1;
+    return 1;
+  }
+};
+
+#endif
