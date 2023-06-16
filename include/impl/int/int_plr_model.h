6fbe3dbe27b48a54600b9a04400e6a1e635fc36e
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-06-15 21:17:59 -0400

    Checkpoint

---
+++ include/impl/int/int_plr_model.h
@@ -0,0 +1,95 @@
+#ifndef INT_PLR_MODEL_H
+#define INT_PLR_MODEL_H
+
+#include "iterator.h"
+#include "model.h"
+#include "plr.h"
+
+template <class T>
+class IntPLRModel : public Model<T> {
+ public:
+  IntPLRModel(PLRModel *model, uint64_t num_keys)
+      : model_(model),
+        plr_segment_index_(0),
+        num_keys_(num_keys),
+        start_offset_(0) {}
+
+  IntPLRModel(PLRModel *model, uint64_t start_offset, uint64_t num_keys)
+      : model_(model),
+        start_offset_(start_offset),
+        plr_segment_index_(0),
+        num_keys_(num_keys) {}
+
+  uint64_t guessPositionMonotone(T target_key) override {
+    std::vector<Segment> &segments = model_->lineSegments_;
+    for (uint64_t i = getPLRLineSegmentIndex(); i < (uint64_t)segments.size();
+         i++) {
+      if (segments[i].last > target_key) {
+        setPLRLineSegmentIndex(i);
+        uint64_t result = std::ceil(target_key * segments[i].k + segments[i].b -
+                                    start_offset_);
+        if (result < 0) {
+          result = 0;
+        }
+        if (result >= num_keys_) {
+          result = num_keys_ - 1;
+        }
+        return result;
+      }
+    }
+    return num_keys_ - 1;
+  };
+
+  uint64_t guessPosition(T target_key) override {
+    std::vector<Segment> &segments = model_->lineSegments_;
+    int32_t left = 0, right = (int32_t)segments.size() - 1;
+    while (left < right) {
+      int32_t mid = (right + left + 1) / 2;
+      if (target_key < segments[mid].x)
+        right = mid - 1;
+      else
+        left = mid;
+    }
+
+    uint64_t result = std::ceil((target_key)*segments[left].k +
+                                segments[left].b - start_offset_);
+    if (result < 0) {
+      result = 0;
+    }
+    if (result >= num_keys_) {
+      result = num_keys_ - 1;
+    }
+    return result;
+  }
+
+  Model<T> *get_model_for_subrange(uint64_t start, uint64_t end) override {
+    return new IntPLRModel(model_, start, end - start);
+  }
+
+ private:
+  PLRModel *model_;
+  uint64_t plr_segment_index_;
+  uint64_t start_offset_;
+  uint64_t num_keys_;
+  uint64_t getPLRLineSegmentIndex() { return plr_segment_index_; };
+  void setPLRLineSegmentIndex(uint64_t value) { plr_segment_index_ = value; }
+};
+
+template <class T>
+class IntPLRModelBuilder : public ModelBuilder<T> {
+ public:
+  IntPLRModelBuilder() : plrBuilder_(new PLRBuilder(10)), num_keys_(0) {}
+  void add(const T &t) override {
+    num_keys_++;
+    plrBuilder_->processKey(t);
+  }
+  IntPLRModel<T> *finish() override {
+    return new IntPLRModel<T>(plrBuilder_->finishTraining(), num_keys_);
+  }
+
+ private:
+  PLRBuilder *plrBuilder_;
+  uint64_t num_keys_;
+};
+
+#endif
---
+++ include/impl/slice/slice_plr_model.h
@@ -0,0 +1,98 @@
+#ifndef SLICE_PLR_MODEL_H
+#define SLICE_PLR_MODEL_H
+
+#include "iterator.h"
+#include "model.h"
+#include "plr.h"
+#include "slice.h"
+
+PLR_SEGMENT_POINT interpret_slice_to_number(const Slice &t) { return 0; }
+
+class SlicePLRModel : public Model<Slice> {
+ public:
+  SlicePLRModel(PLRModel *model, uint64_t num_keys)
+      : model_(model),
+        plr_segment_index_(0),
+        num_keys_(num_keys),
+        start_offset_(0) {}
+
+  SlicePLRModel(PLRModel *model, uint64_t start_offset, uint64_t num_keys)
+      : model_(model),
+        start_offset_(start_offset),
+        plr_segment_index_(0),
+        num_keys_(num_keys) {}
+
+  uint64_t guessPositionMonotone(Slice target_slice_key) override {
+    PLR_SEGMENT_POINT target_key = interpret_slice_to_number(target_slice_key);
+    std::vector<Segment> &segments = model_->lineSegments_;
+    for (uint64_t i = getPLRLineSegmentIndex(); i < (uint64_t)segments.size();
+         i++) {
+      if (segments[i].last > target_key) {
+        setPLRLineSegmentIndex(i);
+        uint64_t result = std::ceil(target_key * segments[i].k + segments[i].b -
+                                    start_offset_);
+        if (result < 0) {
+          result = 0;
+        }
+        if (result >= num_keys_) {
+          result = num_keys_ - 1;
+        }
+        return result;
+      }
+    }
+    return num_keys_ - 1;
+  };
+
+  uint64_t guessPosition(Slice target_slice_key) override {
+    PLR_SEGMENT_POINT target_key = interpret_slice_to_number(target_slice_key);
+    std::vector<Segment> &segments = model_->lineSegments_;
+    int32_t left = 0, right = (int32_t)segments.size() - 1;
+    while (left < right) {
+      int32_t mid = (right + left + 1) / 2;
+      if (target_key < segments[mid].x)
+        right = mid - 1;
+      else
+        left = mid;
+    }
+    uint64_t result = std::ceil((target_key)*segments[left].k +
+                                segments[left].b - start_offset_);
+    if (result < 0) {
+      result = 0;
+    }
+    if (result >= num_keys_) {
+      result = num_keys_ - 1;
+    }
+    return result;
+  }
+
+  SlicePLRModel *get_model_for_subrange(uint64_t start, uint64_t end) override {
+    return new SlicePLRModel(model_, start, end - start);
+  }
+
+ private:
+  PLRModel *model_;
+  uint64_t plr_segment_index_;
+  uint64_t start_offset_;
+  uint64_t num_keys_;
+  uint64_t getPLRLineSegmentIndex() { return plr_segment_index_; };
+  void setPLRLineSegmentIndex(uint64_t value) { plr_segment_index_ = value; }
+};
+
+class SlicePLRModelBuilder : public ModelBuilder<Slice> {
+ public:
+  SlicePLRModelBuilder() : plrBuilder_(new PLRBuilder(10)), num_keys_(0) {}
+  void add(const Slice &slice_key) override {
+    PLR_SEGMENT_POINT t = interpret_slice_to_number(slice_key);
+    num_keys_++;
+    plrBuilder_->processKey(t);
+  }
+  SlicePLRModel *finish() override {
+    return new SlicePLRModel(plrBuilder_->finishTraining(), num_keys_);
+  }
+
+ private:
+  PLRBuilder *plrBuilder_;
+  uint64_t num_keys_;
+};
+
+#endif
