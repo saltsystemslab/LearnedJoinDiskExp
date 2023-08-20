8d6779e0d654c941b3335e92083ee736d0a48b41
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-08-20 22:05:00 +0000

    Updating results

--- benchmark/run_all.sh
+++ benchmark/run_all.sh
@@ -10,3 +10,15 @@ find benchmark/dataset_ur_uint64/*.json | xargs -I {} ./benchmark/benchmark_runn
 
 find benchmark/dataset_ur_16byte/*.jsonnet | xargs -I {} jsonnet {} -o {}.json
 find benchmark/dataset_ur_16byte/*.json | xargs -I {} ./benchmark/benchmark_runner.py --spec={} --repeat=1
+
+find benchmark/dataset_wiki/*.jsonnet | xargs -I {} jsonnet {} -o {}.json
+find benchmark/dataset_fb/*.jsonnet | xargs -I {} jsonnet {} -o {}.json
+find benchmark/dataset_ur_uint64/*.jsonnet | xargs -I {} jsonnet {} -o {}.json
+find benchmark/dataset_ur_16byte/*.jsonnet | xargs -I {} jsonnet {} -o {}.json
+find benchmark/dataset_osm/*.jsonnet | xargs -I {} jsonnet {} -o {}.json
+
+find benchmark/dataset_ur_uint64/*.json | xargs -I {} ./benchmark/benchmark_runner.py --spec={} --repeat=4
+find benchmark/dataset_ur_16byte/*.json | xargs -I {} ./benchmark/benchmark_runner.py --spec={} --repeat=4
+find benchmark/dataset_fb/*.json | xargs -I {} ./benchmark/benchmark_runner.py --spec={} --repeat=4
+find benchmark/dataset_wiki/*.json | xargs -I {} ./benchmark/benchmark_runner.py --spec={} --repeat=4
+
