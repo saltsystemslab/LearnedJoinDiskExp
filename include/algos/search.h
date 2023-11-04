5b5cb9377e6472d88da8136661493c87cc9c8a0f
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-11-04 02:45:18 +0000

    Implement Binary Search

--- include/algos/search.h
+++ include/algos/search.h
@@ -18,6 +18,7 @@ class SearchStrategy {
     virtual SearchResult search(Window<T> window, T Key, Bounds bound) = 0;
 };
 
+// TODO(chesetti): Templatize. Right now hardcoded to 64 byte integer keys.
 class LinearSearch: public SearchStrategy<KVSlice> {
 public:
   SearchResult search(Window<KVSlice> window, KVSlice kv, Bounds bound) override {
@@ -58,6 +59,85 @@ public:
     };
   }
 };
+
+// TODO(chesetti): Templatize, right now hardcoded for 8 + 8 64 byte keys.
+class BinarySearch : public SearchStrategy<KVSlice> {
+private:
+  inline size_t bit_floor(size_t i) {
+    constexpr int num_bits = sizeof(i) * 8;
+    return size_t(1) << (num_bits - std::countl_zero(i) - 1);
+  }
+
+  inline size_t bit_ceil(size_t i) {
+    constexpr int num_bits = sizeof(i) * 8;
+    return size_t(1) << (num_bits - std::countl_zero(i - 1));
+  }
+
+  uint64_t find_lower_bound(char *buf, uint64_t len_in_bytes, uint64_t key_value) {
+    uint64_t start = 0;
+    int kvSize = 16;
+    uint64_t end = (len_in_bytes) / (kvSize);
+    uint64_t len = end - start;
+    uint64_t step = bit_floor(len);
+    uint64_t cur = *(uint64_t *)(buf + (kvSize) * (step + start));
+    if (step != len && (cur < key_value)) {
+      len -= step + 1;
+      if (len == 0) {
+        return 0;
+      }
+      step = bit_ceil(len);
+      start = end - step;
+    }
+    for (step /= 2; step != 0; step /= 2) {
+      cur = *(uint64_t *)(buf + (kvSize) * (step + start));
+      if (cur < key_value)
+        start += step;
+    }
+    cur = *(uint64_t *)(buf + (kvSize) * (step + start));
+    start = start + (cur < key_value);
+    return start;
+  }
+
+
+
+public:
+  SearchResult search(Window<KVSlice> window, KVSlice kv, Bounds bound) override {
+    if (bound.lower >= bound.upper) {
+      return {
+        bound.lower,
+        false,
+        false
+      };
+    }
+    int kvSize = 16;
+    uint64_t key_value = *(uint64_t *)(kv.data());
+    uint64_t last_key = *(uint64_t *)(window.buf + window.buf_len - kvSize);
+    if (last_key < key_value) {
+      return {
+        window.hi_idx,
+        false, // found
+        true  // continue
+      };
+    }
+    uint64_t lower_bound = find_lower_bound(window.buf, window.buf_len, key_value);
+    uint64_t lower_bound_key = *(uint64_t *)(window.buf + lower_bound * kvSize);
+    if (lower_bound_key == key_value) {
+      return {
+        window.lo_idx + lower_bound,
+        true, // found
+        false // continue
+      };
+    } else {
+      return {
+        window.lo_idx + lower_bound,
+        false, // found
+        false // continue
+      };
+    }
+  }
+};
+
+
 
 }
 #endif
