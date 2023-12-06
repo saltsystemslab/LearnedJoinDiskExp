32757fd7cbfc811e226d9a321925a524a9573576
Yuvaraj Chesetti <yuvaraj.chesetti@gmail.com>
2023-12-06 07:31:42 +0000

    Updating results

--- experiments/epsilon_vs_duration/run.py
+++ experiments/epsilon_vs_duration/run.py
@@ -6,7 +6,7 @@ from absl import flags
 
 FLAGS = flags.FLAGS
 
-flags.DEFINE_integer("repeat", 3, "")
+flags.DEFINE_integer("repeat", 2, "")
 flags.DEFINE_integer("worker_threads", 5, "")
 flags.DEFINE_bool("clear_inputs", False, "")
 flags.DEFINE_bool("check_checksum", True, "")
@@ -15,7 +15,35 @@ flags.DEFINE_bool("skip_input", False, "")
 datasets = {
     "fb": {
         "source": "/home/chesetti/sosd-data/fb_200M_uint64",
-        "num_keys": 20000000
+        "num_keys": 200000000
+    },
+    "lognormal": {
+        "source": "/home/chesetti/sosd-data/lognormal_200M_uint64",
+        "num_keys": 200000000
+    },
+    "uniform_sparse": {
+        "source": "/home/chesetti/sosd-data/uniform_sparse_200M_uint64",
+        "num_keys": 200000000
+    },
+    "uniform_dense": {
+        "source": "/home/chesetti/sosd-data/uniform_dense_200M_uint64",
+        "num_keys": 200000000
+    },
+    "normal": {
+        "source": "/home/chesetti/sosd-data/normal_200M_uint64",
+        "num_keys": 200000000
+    },
+    "wiki": {
+        "source": "/home/chesetti/sosd-data/wiki_ts_200M_uint64",
+        "num_keys": 200000000
+    },
+    "osm": {
+        "source": "/home/chesetti/sosd-data/osm_cellids_800M_uint64",
+        "num_keys": 800000000
+    },
+    "books": {
+        "source": "/home/chesetti/sosd-data/books_800M_uint64",
+        "num_keys": 800000000
     },
 }
 
@@ -23,7 +51,7 @@ def main(argv):
     benchmark_script = "./scripts/benchmark.py"
     test_config = "--spec=./experiments/epsilon_vs_duration/join.jsonnet"
     for name, dataset in datasets.items():
-        for thread in [2, 4]:
+        for thread in [2, 4, 8, 16]:
             args = [benchmark_script, test_config]
             args.append(f"--threads={thread}")
             args.append(f"--repeat={FLAGS.repeat}")
@@ -35,7 +63,6 @@ def main(argv):
             args.append(f'--sosd_num_keys={dataset["num_keys"]}')
             args.append(f'--workers={FLAGS.worker_threads}')
             args.append(f'--skip_input={FLAGS.skip_input}')
-            args.append(f'--nouse_numactl')
             print(args)
             subprocess.run(args)
 
